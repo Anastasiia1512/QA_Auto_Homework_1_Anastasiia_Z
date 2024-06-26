f604c69826edf0c44585f59fbb256f698e709fa6
Anastasiia1512 <anastasiazuravleva900@gmail.com>
2024-06-26 15:49:38 +0200

    Revert "Create SELECT_with_JOIN_queries_Demo.sql"

--- SELECT_with_JOIN_queries_Demo.sql
+++
@@ -1,7 +0,0 @@
-
-SELECT `Visits`.`VisitDate`, `Visits`.`Reason`, `MedicalRecords`.`Diagnosis`, `MedicalRecords`.`Treatment`, `Cats`.`Name` AS CatName, `Cats`.`Breed`, `Owners`.`Name` AS OwnerName,`Owners`.`Phone`
-FROM `Visits`
-JOIN `MedicalRecords` ON `Visits`.`VisitID` = `MedicalRecords`.`VisitID`
-JOIN `Cats` ON `Visits`.`CatID` = `Cats`.`CatID`
-JOIN `Owners` ON `Cats`.`OwnerID` = `Owners`.`OwnerID`
-WHERE `VisitDate` > '2024-03-01';
